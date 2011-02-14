21747537b7d42a722868ed59ca1943d1a72353a7
Swann Perarnau <swann.perarnau@imag.fr>
2011-02-14 12:12:11 +0100

    [fix] reorder commands

--- ccontrol/autogen.sh
+++ ccontrol/autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -x
-sh version.sh
-aclocal -I m4 || exit 1
-autoheader || exit 1
-libtoolize || exit 1
-automake --add-missing --copy || exit 1
 # autoconf needs build-aux directory
 mkdir -p build-aux || exit 1
+sh version.sh
+# libtoolize generates m4 dir
+libtoolize || exit 1
+aclocal -I m4 || exit 1
+autoheader || exit 1
+automake --add-missing --copy || exit 1
 autoconf || exit 1
